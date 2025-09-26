34384f42b46952a836a9cbf6be7276cef1a4a43c
alonn201 <145072654+alonn201@users.noreply.github.com>
2025-09-26 16:40:02 -0700

    main menu and backend for multiplayer

---
+++ scripts/control.gd
@@ -0,0 +1,38 @@
+extends Control
+
+@onready var main_menu: PanelContainer = $CanvasLayer/MainMenu
+@onready var adress: LineEdit = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/Address
+
+
+const PORT = 1771
+var enet_peer = ENetMultiplayerPeer.new()
+
+func _on_host_pressed() -> void:
+	
+	main_menu.hide()
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	
+	upnp_setup()
+
+func _on_join_pressed() -> void:
+	
+	main_menu.hide()
+	enet_peer.create_client("127.0.0.1",PORT)
+	multiplayer.multiplayer_peer = enet_peer
+
+func upnp_setup():
+	var upnp = UPNP.new()
+	
+	var discover_result = upnp.discover()
+	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Discover Failed! Error %s" % discover_result)
+	
+	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), \
+		"UPNP Invalid Gateway")
+	
+	var map_result = upnp.add_port_mapping(PORT)
+	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Port Mapping Failed! Error %s" % map_result)
+	
+	print("Success! Join Address:" % upnp.query_external_address())
